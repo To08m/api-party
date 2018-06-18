e6909fa184b5e607d853ee091ab1514631418d44
PeterGawecki <pgawecki@hotmail.com>
2018-06-18 10:17:46 -0400

    add github component with form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
+
 import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
@@ -10,16 +12,20 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
-
-        <ul className = "navLinks">
+        <ul className="navLinks">
           <li>
-            <NavLink to='/github'>Github API</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
-
         <Switch>
-          <Route path='/github' render={() => <h1>github!</h1>}/>
-          <Route render={()=>(<p>to get started, click one of the links</p>)}/>
+          <Route path="/github" component={Github} />
+          <Route
+            render={
+              () => (
+                <p>To get started, click one of the links above</p>
+              )
+            }
+          />
         </Switch>
       </div>
     )
